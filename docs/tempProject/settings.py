4ef0446242e54a0d791373f19cd4c83b35cfb5ae
Akraradet Sinsamersuk <akraradets@gmail.com>
2022-03-08 13:40:47 +0700

    update dockerfile

--- docs/tempProject/settings.py
+++ docs/tempProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mghdb-jz!r@m3j5tv^mnq#(xce0yy-6c4wzh#=qmq51yzw7tb+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
